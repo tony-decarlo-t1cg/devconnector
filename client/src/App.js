cd038ec46ae2b4899d844ab2eaf8059736be0b73
Tony DeCarlo <anthony.decarlo@t1cg.com>
2018-11-29 23:50:34 -0500

    Redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,25 @@ import Landing from './components/layout/Landing';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -16,7 +16,7 @@ class Landing extends Component {
                   from other developers
                 </p>
                 <hr />
-                <Link href="/register" className="btn btn-lg btn-info mr-2">
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
                 </Link>
                 <Link to="/login" className="btn btn-lg btn-light">
