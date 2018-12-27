16dddaaaaad1f07294252e99503fd733883e1c6b
Tony DeCarlo <anthony.decarlo@t1cg.com>
2018-12-26 20:00:48 -0500

    profile actions and edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import { clearCurrentProfile } from './actions/profileActions';
 import CreateProfile from './components/create-profile/CreateProfile';
 
 import PrivateRoute from './components/common/PrivateRoute';
+import EditProfile from './components/edit-profile/EditProfile';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -55,6 +56,13 @@ class App extends Component {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
                 />
               </Switch>
             </div>
